353783fc1184cf9087a54937ea3ac144978f286b
Kevin Towe <kevintowe27@gmail.com>
2019-12-08 17:12:04 -0600

    routing module

---
+++ apps/its-your-practice/src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {
+  redirectUnauthorizedTo,
+  redirectLoggedInTo,
+  canActivate,
+  AngularFireAuthGuard
+} from '@angular/fire/auth-guard';
+
+const unAuthUserGoesToLanding = () => redirectUnauthorizedTo(['landing']);
+const authUserGoesToHome = () => redirectLoggedInTo(['']);
+
+const appRoutes: Routes = [
+  {
+    path: 'landing',
+    loadChildren: () =>
+      import('@its-your-practice/feature/landing').then(
+        mod => mod.FeatureLandingModule
+      ),
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: authUserGoesToHome }
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('@its-your-practice/feature/home').then(
+        mod => mod.FeatureHomeModule
+      ),
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: unAuthUserGoesToLanding }
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    )
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
