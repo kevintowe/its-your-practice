5331b96639785de3f4b3482880069abd0955fdb9
Kevin Towe <kevintowe27@gmail.com>
2019-12-08 17:13:42 -0600

    angular fire

--- apps/its-your-practice/src/app/app.module.ts
+++ apps/its-your-practice/src/app/app.module.ts
@@ -4,6 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Angular Fire(https://github.com/angular/angularfire)
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireAuthGuardModule } from '@angular/fire/auth-guard';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, BrowserAnimationsModule],
