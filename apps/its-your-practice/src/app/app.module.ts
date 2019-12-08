aafb69c454a91d742d8a17521d840f74d4c890bc
Kevin Towe <kevintowe27@gmail.com>
2019-12-08 17:14:35 -0600

    import app routing module

--- apps/its-your-practice/src/app/app.module.ts
+++ apps/its-your-practice/src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-
 // Angular Fire(https://github.com/angular/angularfire)
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireAuthGuardModule } from '@angular/fire/auth-guard';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
