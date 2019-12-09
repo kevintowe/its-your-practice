727ea249ba23d6ad4ea51d27403ffe806760775c
Kevin Towe <kevintowe27@gmail.com>
2019-12-08 18:43:24 -0600

    ngrx

--- apps/its-your-practice/src/app/app.module.ts
+++ apps/its-your-practice/src/app/app.module.ts
@@ -8,6 +8,13 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireAuthGuardModule } from '@angular/fire/auth-guard';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
+// NgRx
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule, RouterState } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { ROOT_REDUCERS, metaReducers } from './reducers';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -22,7 +29,18 @@ import { environment } from '../environments/environment';
     AngularFireAuthModule,
     AngularFireAuthGuardModule,
     AngularFirestoreModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(ROOT_REDUCERS, {
+      metaReducers,
+      runtimeChecks: {}
+    }),
+    StoreRouterConnectingModule.forRoot({
+      routerState: RouterState.Minimal
+    }),
+    StoreDevtoolsModule.instrument({
+      name: 'Its Your Practice'
+    }),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
